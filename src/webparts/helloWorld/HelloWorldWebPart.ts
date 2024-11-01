02326b019cf53c7f1c6abfe880e727e11ead0c2e
haixinhne <lehai1612@gmail.com>
2024-11-01 08:26:27 +0700

    lan 7

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -12,14 +12,9 @@ import {
   SPHttpClientResponse,
   ISPHttpClientOptions,
 } from "@microsoft/sp-http";
+import { __metadata } from "tslib";
 
-const options: ISPHttpClientOptions = {
-  headers: {
-    accept: "application/json; odata=verbose",
-    "content-type": "application/json; odata=verbose",
-  },
-};
-
+//Url file Excel
 const excelUrl = "/sites/QMS/Shared Documents/Book1.xlsx";
 
 export interface IHelloWorldWebPartProps {
@@ -48,20 +43,23 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
      <div class="${styles.welcome}">    
         <h2>Hello, ${escape(this.context.pageContext.user.displayName)}</h2>
        <div>${this._environmentMessage}</div>
+       <input type="text" class="${
+         styles.qms_input
+       }" id="titleSharepointList", " placeholder="Title sharepoint list" />
         <button class="${
           styles.qms_button
         }" id="createFolderButton">Create Folder</button>
         <button class="${
           styles.qms_button
-        }" id="createSharepointButton">Create Sharepoint</button>
+        }" id="createSharepointButton">Create Sharepoint</button>       
      </div>
      </div>     
      </section>`;
 
-    //Hành động nhấn nút
+    //Click button
     const buttonClick = this.domElement.querySelector("#createFolderButton");
     if (buttonClick) {
-      buttonClick.addEventListener("click", () => this._onClickButton());
+      buttonClick.addEventListener("click", () => this.onCreateFolder());
     }
 
     const buttonClickCreateSharepoint = this.domElement.querySelector(
@@ -69,15 +67,14 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
     );
     if (buttonClickCreateSharepoint) {
       buttonClickCreateSharepoint.addEventListener("click", () =>
-        this._onClickButtonCreateSharepoint()
+        this.onClickButtonCreateSharepoint()
       );
     }
-
-    this._renderListAsync();
+    this.renderListAsync();
   }
 
   protected onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then((message) => {
+    return this.getEnvironmentMessage().then((message) => {
       this._environmentMessage = message;
     });
   }
@@ -98,7 +95,7 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
     }
   }
 
-  private _renderListAsync(): void {
+  private renderListAsync(): void {
     this._getListData()
       .then((response) => {
         this._renderList(response.value);
@@ -136,25 +133,35 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
   }
 
   //Hàm đọc nội dung file excel (lấy tên các cột)
-  private readFileExcelFromSharePoint(fileContent: ArrayBuffer): string[] {
+  private readFileExcelFromSharePoint(fileContent: ArrayBuffer): {
+    nameColumnSharepoint: string[];
+    nameItems: Record<string, any>[];
+  } {
     const data = new Uint8Array(fileContent);
     const workbook = XLSX.read(data, { type: "array" });
     const firstSheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[firstSheetName];
     const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-    const nameColumnSharepointList = (jsonData[0] as any[]).filter(Boolean);
-    return nameColumnSharepointList;
+    const nameColumnSharepoint = (jsonData[0] as any[]).filter(Boolean);
+    const nameItems = jsonData.slice(1).map((row: any[]) => {
+      const rowObject: Record<string, any> = {};
+      nameColumnSharepoint.forEach((colName, index) => {
+        rowObject[colName] = row[index] || null;
+      });
+      return rowObject;
+    });
+    console.log("Items", nameItems);
+    return { nameColumnSharepoint, nameItems };
   }
 
   // Tạo SharePoint list
-  private async createSharePointList(listName: string): Promise<any> {
+  private createSharePointList(listName: string): Promise<any> {
     const body = JSON.stringify({
       __metadata: { type: "SP.List" },
       BaseTemplate: 100,
       Title: listName,
     });
-
-    const optionsJson: ISPHttpClientOptions = {
+    const optionsHTTP: ISPHttpClientOptions = {
       headers: {
         Accept: "application/json;odata=verbose",
         "Content-Type": "application/json;odata=verbose",
@@ -163,11 +170,11 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
       body: body,
     };
 
-    return await this.context.spHttpClient
+    return this.context.spHttpClient
       .post(
         `${this.context.pageContext.web.absoluteUrl}/_api/web/lists`,
         SPHttpClient.configurations.v1,
-        optionsJson
+        optionsHTTP
       )
       .then((response: SPHttpClientResponse) => {
         if (response.ok) {
@@ -182,7 +189,7 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
         console.error("Error creating:", error);
       });
   }
-
+  // Tạo cột Sharepoint list
   private async createColumnInSharePoint(
     listName: string,
     columnName: string
@@ -190,10 +197,9 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
     const body = JSON.stringify({
       __metadata: { type: "SP.Field" },
       Title: columnName,
-      FieldTypeKind: 2, // FieldTypeKind 2 tương ứng với cột kiểu Văn bản
+      FieldTypeKind: 2,
     });
-
-    const optionsJson: ISPHttpClientOptions = {
+    const optionsHTTP: ISPHttpClientOptions = {
       headers: {
         Accept: "application/json;odata=verbose",
         "Content-Type": "application/json;odata=verbose",
@@ -204,9 +210,9 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
 
     return await this.context.spHttpClient
       .post(
-        `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${listName}')/fields`,
+        `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${listName}')/fields`,
         SPHttpClient.configurations.v1,
-        optionsJson
+        optionsHTTP
       )
       .then((response: SPHttpClientResponse) => {
         if (response.ok) {
@@ -222,27 +228,114 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
       });
   }
 
-  // Click Tạo SharePoint List
-  private _onClickButtonCreateSharepoint(): void {
-    const listNameSharePoint = "QMS_Excel";
+  //Hàm caps lock chữ cái đầu
+  private capsLocksFirstLetter(text: string): string {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  }
 
+  //Tạo items
+  private async createItemsInSharePointList(
+    listName: string,
+    itemData: any
+  ): Promise<void> {
+    const capsLockListName = this.capsLocksFirstLetter(listName);
+    const bodyObject: Record<string, any> = {
+      __metadata: {
+        type: `SP.Data.${capsLockListName}ListItem`,
+      },
+      Title: itemData.Title || "QMS",
+    };
+
+    for (const key in itemData) {
+      if (itemData.hasOwnProperty(key)) {
+        bodyObject[key] = String(itemData[key] || "");
+      }
+    }
+    const body = JSON.stringify(bodyObject);
+    const optionsHTTP: ISPHttpClientOptions = {
+      headers: {
+        Accept: "application/json;odata=verbose",
+        "Content-Type": "application/json;odata=verbose",
+        "odata-version": "",
+      },
+      body: body,
+    };
+
+    return await this.context.spHttpClient
+      .post(
+        `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${listName}')/items`,
+        SPHttpClient.configurations.v1,
+        optionsHTTP
+      )
+      .then((response: SPHttpClientResponse) => {
+        if (!response.ok) {
+          return response.json().then((errorResponse) => {
+            console.error("Error response:", errorResponse);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error adding item:", error);
+      });
+  }
+
+  // Click Tạo SharePoint list, add column, add items
+  private onClickButtonCreateSharepoint(): void {
+    const listNameSharePoint = (
+      document.getElementById("titleSharepointList") as HTMLInputElement
+    ).value;
+
+    if (!listNameSharePoint) {
+      alert("Please enter a name for the SharePoint list!");
+
+      return;
+    }
+    //Tạo sharepoint list
     this.createSharePointList(listNameSharePoint)
       .then(() => {
+        (
+          document.getElementById("titleSharepointList") as HTMLInputElement
+        ).value = "";
         console.log(`Created successfully: ${listNameSharePoint}`);
-
-        return this.getFileExcelFromSharePoint(excelUrl);
       })
+      //Tạo cột từ file excel
+      .then(() => this.getFileExcelFromSharePoint(excelUrl))
       .then((fileContent: ArrayBuffer) => {
-        const columnNameExcel = this.readFileExcelFromSharePoint(fileContent);
-
-        const createColumnName = columnNameExcel.map((columnName) => {
-          return this.createColumnInSharePoint(listNameSharePoint, columnName);
-        });
-
-        return Promise.all(createColumnName);
+        const { nameColumnSharepoint, nameItems } =
+          this.readFileExcelFromSharePoint(fileContent);
+        return nameColumnSharepoint
+          .reduce((promise, createColumn) => {
+            return promise.then(() => {
+              console.log(`Adding column: ${createColumn}`);
+              return this.createColumnInSharePoint(
+                listNameSharePoint,
+                createColumn
+              ).catch((error) => {
+                console.error(`Error adding column "${createColumn}":`, error);
+              });
+            });
+          }, Promise.resolve())
+          .then(() => ({
+            nameColumnSharepoint,
+            nameItems,
+          }));
+      })
+      //Tạo items
+      .then(({ nameColumnSharepoint, nameItems }) => {
+        console.log("All columns created successfully.");
+        console.log("Input data", nameItems);
+        return nameItems.reduce((promise, itemData) => {
+          const itemObject = nameColumnSharepoint.reduce((obj, columnName) => {
+            obj[columnName] = itemData[columnName] || null;
+            return obj;
+          }, {} as Record<string, any>);
+          return promise.then(() =>
+            this.createItemsInSharePointList(listNameSharePoint, itemObject)
+          );
+        }, Promise.resolve());
       })
       .then(() => {
-        console.log("All columns created successfully.");
+        console.log("All items created successfully.");
       })
       .catch((error) => {
         console.error("Click Created Error:", error);
@@ -250,9 +343,12 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
   }
 
   //Tạo các thư mục con từ sharepoint------------------------------------------------------------------------------------------------------------------------------
-  // Hàm lấy dữ liệu từ SharePoint list (Lấy tên thư mục cần tạo theo tên của các item (hàng ngang))
-  private _getFileFromSharePoint(): Promise<string[]> {
-    const listUrl = `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('TestExcel')/items`;
+  // Hàm lấy data từ SharePoint list (Lấy tên thư mục là 1 cột ở sharepoint list)
+  private getFileFromSharePoint(): Promise<string[]> {
+    const listNameSharePoint = (
+      document.getElementById("titleSharepointList") as HTMLInputElement
+    ).value;
+    const listUrl = `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('${listNameSharePoint}')/items`;
 
     return this.context.spHttpClient
       .get(listUrl, SPHttpClient.configurations.v1)
@@ -260,8 +356,9 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
         return response.json();
       })
       .then((data: any) => {
+        console.log("Data", data);
         const folderNames = data.value
-          .map((item: any) => item.Title) // item.tên cột chứa tên thư mục
+          .map((item: any) => item.Name) //Cột lấy tên folder
           .filter(Boolean);
 
         return folderNames;
@@ -273,15 +370,21 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
   }
 
   // Hàm tạo subfolder
-  private _createSubfolder(
+  private createSubfolder(
     parentFolderName: string,
     subFolderName: string
   ): Promise<any> {
+    const optionsHTTP: ISPHttpClientOptions = {
+      headers: {
+        accept: "application/json; odata=verbose",
+        "content-type": "application/json; odata=verbose",
+      },
+    };
     return this.context.spHttpClient
       .post(
         `${this.context.pageContext.web.absoluteUrl}/_api/web/folders/add('Shared Documents/${parentFolderName}/${subFolderName}')`,
         SPHttpClient.configurations.v1,
-        options
+        optionsHTTP
       )
       .then((response: SPHttpClientResponse) => {
         return response.json();
@@ -290,34 +393,44 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
   }
 
   //Hàm tạo folder
-  private _createFolder(folderName: string): Promise<any> {
+  private createFolder(folderName: string): Promise<any> {
+    const optionsHTTP: ISPHttpClientOptions = {
+      headers: {
+        accept: "application/json; odata=verbose",
+        "content-type": "application/json; odata=verbose",
+      },
+    };
     return this.context.spHttpClient
       .post(
         `${this.context.pageContext.web.absoluteUrl}/_api/web/folders/add('Shared Documents/${folderName}')`,
         SPHttpClient.configurations.v1,
-        options
+        optionsHTTP
       )
       .then((response: SPHttpClientResponse) => {
         return response.json();
       })
       .then(() => {
-        // Tạo 3 folder con bên trong folder chính
         return Promise.all([
-          this._createSubfolder(folderName, "00_ESSENTIAL"),
-          this._createSubfolder(folderName, "01_WORK"),
-          this._createSubfolder(folderName, "02_SUBMIT"),
+          this.createSubfolder(folderName, "00_ESSENTIAL"),
+          this.createSubfolder(folderName, "01_WORK"),
+          this.createSubfolder(folderName, "02_SUBMIT"),
         ]);
       })
       .catch(() => {});
   }
 
-  // Hành động nhấn nút tạo folder
-  private _onClickButton(): void {
-    this._getFileFromSharePoint()
+  //Click Tạo folder
+  private onCreateFolder(): void {
+    this.getFileFromSharePoint()
       .then((folderNames: string[]) => {
         folderNames.forEach((folderName: string) => {
-          this._createFolder(folderName)
+          this.createFolder(folderName)
             .then(() => {
+              (
+                document.getElementById(
+                  "titleSharepointList"
+                ) as HTMLInputElement
+              ).value = "";
               console.log(`Created successfully: ${folderName}`);
             })
             .catch((error) => {
@@ -330,7 +443,7 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
       });
   }
 
-  private _getEnvironmentMessage(): Promise<string> {
+  private getEnvironmentMessage(): Promise<string> {
     if (!!this.context.sdks.microsoftTeams) {
       //running in Teams, office.com or Outlook
       return this.context.sdks.microsoftTeams.teamsJs.app
